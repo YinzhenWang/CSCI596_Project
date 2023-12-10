dbc7feb7a589197684149d9b45f819aa78e3465a
HarrisonKim <kimheavy@yahoo.com>
2023-12-10 13:02:57 -0800

    [Increase Contrast] Refactor line width to non-linear with weight

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from models import run_model
 from draw import initialize_window, render_image
 import sys
+import argparse
 
 def run(text,model_name,layer,head):
     attentions, tokens = run_model(text,model_name,layer, head)
@@ -11,15 +12,13 @@ def run(text,model_name,layer,head):
     return image_name
 
 if __name__ == "__main__":
-    text = sys.argv[1]
-    model_name = sys.argv[2]
-    layer = int(sys.argv[3])
-    head = int(sys.argv[4])
-    image_name = run(text,model_name,layer,head)
+    # python run.py -t "The cat sat on the mat" -m "dslim/bert-base-NER" -l 11 -he 4
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--text", "-t", help="text to be processed")
+    parser.add_argument("--model_name", "-m", help="model name")
+    parser.add_argument("--layer", "-l", help="layer")
+    parser.add_argument("--head", "-he", help="head")
+
+    args = parser.parse_args()
+    image_name = run(args.text,args.model_name,int(args.layer),int(args.head))
     print(image_name)
-# text = "I am a student in USC majoring in computer science"
-# # model = "dslim/bert-base-NER"
-# model = "bert-base-uncased"
-# layer = 11
-# head = 4
-# run(text,model,layer,head)
